c380164fd4a0114dc30726e386dd75cc3314d24e
Muhammad Gad <106442148+GADMuhammad@users.noreply.github.com>
2025-07-18 09:34:27 +0300

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -17,7 +17,11 @@ const Header = () => {
 
   return (
     <header className="bg-header border-border mx-30 my-12 flex flex-wrap justify-between gap-6 rounded-2xl border-2 border-solid px-4 py-3 shadow-md transition-colors duration-300 max-sm:flex-col">
-      <img src="/images/logo.svg" alt="LOGO" className="mr-auto" />
+      <img
+        src={`/images/logo${isDark ? "-dark" : ""}.svg`}
+        alt="LOGO"
+        className="mr-auto"
+      />
       <button
         onClick={() => toggleTheme()}
         className="bg-btn-bg w-fit cursor-pointer rounded-xl p-2 max-sm:mx-auto"
